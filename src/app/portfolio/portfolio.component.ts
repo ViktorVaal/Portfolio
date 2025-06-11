e924c80ee98886cdc969d930d636169f115d797e
ViktorVaal <vaal.viktor@web.de>
2025-06-11 17:54:55 +0200

    projects desktop designe finished

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -3,11 +3,18 @@ import { LandingPageComponent } from '../landing-page/landing-page.component';
 import { AbouteMeComponent } from "./aboute-me/aboute-me.component";
 import { MovingCursorShadowComponent } from "../moving-cursor-shadow/moving-cursor-shadow.component";
 import { TechnologiesComponent } from './technologies/technologies.component';
+import { ProjectsComponent } from './projects/projects.component';
 
 @Component({
   selector: 'app-portfolio',
   standalone: true,
-  imports: [LandingPageComponent, AbouteMeComponent, MovingCursorShadowComponent, TechnologiesComponent],
+  imports: [
+    LandingPageComponent,
+    AbouteMeComponent,
+    MovingCursorShadowComponent,
+    TechnologiesComponent,
+    ProjectsComponent
+  ],
   templateUrl: './portfolio.component.html',
   styleUrl: './portfolio.component.scss'
 })
