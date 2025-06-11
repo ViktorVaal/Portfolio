9b363da9fa2174be598b3dfcf633c8b91cdf7a81
ViktorVaal <vaal.viktor@web.de>
2025-06-11 21:52:41 +0200

    refactored code

--- src/app/portfolio/projects/projects.component.ts
+++ src/app/portfolio/projects/projects.component.ts
@@ -1,12 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
+  projects = [
+    {name: 'Join', technologies: ['Angular', 'TypeScript', 'HMTL', 'CSS', 'Firebase']},
+    {name: 'El-Pollo-Loco', technologies: [ 'HMTL', 'CSS', 'JavaScript']},
+    {name: 'DA-Bubble', technologies: [ 'Angular', 'Firebase', 'TypeScript']}
+  ]
 
+  openProjectOverlay($index: number) {
+    
+  }
 }
