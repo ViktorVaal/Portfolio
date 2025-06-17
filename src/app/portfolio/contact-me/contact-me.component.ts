8f2aac2fd0d73dcf9d42f3ac36443e40214047bb
ViktorVaal <vaal.viktor@web.de>
2025-06-17 17:46:51 +0200

    contact me section layout

--- src/app/portfolio/contact-me/contact-me.component.ts
+++ src/app/portfolio/contact-me/contact-me.component.ts
@@ -1,12 +1,31 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-me',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, CommonModule],
   templateUrl: './contact-me.component.html',
   styleUrl: './contact-me.component.scss'
 })
 export class ContactMeComponent {
 
+  privacyPolicyChecked: boolean = false;
+  fullName: boolean = false;
+  email: boolean = false;
+  message: boolean = false;
+  
+  onSubmit() {
+    console.log('läuft');
+    
+  }
+
+  toggleCheckBox() {
+    if (this.privacyPolicyChecked === false) {
+      this.privacyPolicyChecked = true;
+    } else {
+      this.privacyPolicyChecked = false;
+    }
+  }
 }
